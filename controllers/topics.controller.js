4fe3df961031572f2a4bf7c67dc985dd58414570
Damian Martin <dmartinr2d2@gmail.com>
2022-08-01 17:23:32 +0100

    this commit will remove articles functions

--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -1,5 +1,4 @@
-const { response } = require('../app');
-const { selectTopics } = require('../models/topics.model');
+const { selectTopics, selectArticleById } = require('../models/topics.model');
 
 exports.getTopics = (req, res, next) => {
 	selectTopics().then((topics) => res.status(200).send({ topics })).catch(next);
