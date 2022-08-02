267d715a3420167d37f35859b895ceaa2d7d5927
Damian Martin <dmartinr2d2@gmail.com>
2022-08-02 16:05:19 +0100

    this commit will add updatedVotesByArticleID query function

--- models/articles.model.js
+++ models/articles.model.js
@@ -13,3 +13,15 @@ exports.selectArticleById = (id) => {
 			return rows[0];
 		});
 };
+
+exports.updatedVotesByArticleById = (id, update) => {
+	return db
+		.query(`UPDATE articles SET votes = votes + $2 WHERE article_id = $1 RETURNING*;`, [ id, update ])
+		.then(({ rows }) => {
+			console.log(rows);
+			if (rows.length === 0) {
+				return Promise.reject({ status: 404, msg: 'Article not found' });
+			}
+			return rows[0];
+		});
+};
