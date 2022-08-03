34a87570ef6e36e7a8724bf9a49cfde01a215e77
Damian Martin <dmartinr2d2@gmail.com>
2022-08-03 13:03:00 +0100

    this commit will add tests for /api/articles endpoint. Will also restructure tests to be more readable in the terminal

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,131 +7,162 @@ const testData = require('../db/data/test-data/index');
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
 
-describe('/api/topics', () => {
-	describe('GET', () => {
-		test('Status 200: returns an array of topics', () => {
-			return request(app).get('/api/topics').expect(200).then((response) => {
-				expect(response.body.topics).toEqual(expect.any(Array));
+describe('GOOD ENDPOINTS\n', () => {
+	describe('/api/topics', () => {
+		describe('GET', () => {
+			test('Status 200: returns an array of topics', () => {
+				return request(app).get('/api/topics').expect(200).then((response) => {
+					expect(response.body.topics).toEqual(expect.any(Array));
+				});
+			});
+			test('Status 200: returns an array of objects with the correct properties', () => {
+				return request(app).get('/api/topics').expect(200).then((response) => {
+					const allTopics = response.body.topics;
+					allTopics.forEach((topic) => {
+						expect(topic.hasOwnProperty('slug')).toBe(true);
+						expect(topic.hasOwnProperty('description')).toBe(true);
+					});
+				});
 			});
 		});
-		test('Status 200: returns an array of objects with the correct properties', () => {
-			return request(app).get('/api/topics').expect(200).then((response) => {
-				const allTopics = response.body.topics;
-				allTopics.forEach((topic) => {
-					expect(topic.hasOwnProperty('slug')).toBe(true);
-					expect(topic.hasOwnProperty('description')).toBe(true);
+	});
+	describe('/api/articles', () => {
+		describe('GET', () => {
+			test('Status 200: returns an array of articles', () => {
+				return request(app).get('/api/articles').expect(200).then((response) => {
+					expect(response.body.articles).toEqual(expect.any(Array));
+				});
+			});
+			test('Status 200: returns an array of objects with the correct properties and value types', () => {
+				return request(app).get('/api/articles').expect(200).then((response) => {
+					const allArticles = response.body.articles;
+					allArticles.forEach((article) => {
+						expect(article).toHaveProperty('author');
+						expect(article).toHaveProperty('title');
+						expect(article).toHaveProperty('article_id');
+						expect(article).toHaveProperty('topic');
+						expect(article).toHaveProperty('created_at');
+						expect(article).toHaveProperty('votes');
+						expect(article).toHaveProperty('comment_count');
+						expect(article.author).toEqual(expect.any(String));
+						expect(article.title).toEqual(expect.any(String));
+						expect(article.article_id).toEqual(expect.any(Number));
+						expect(article.topic).toEqual(expect.any(String));
+						expect(article.created_at).toEqual(expect.any(String));
+						expect(article.votes).toEqual(expect.any(Number));
+						expect(article.comment_count).toEqual(expect.any(Number));
+					});
+				});
+			});
+		});
+	});
+	describe('/api/articles/:article_id', () => {
+		describe('GET', () => {
+			test('returns an article by an ID number', () => {
+				return request(app).get('/api/articles/1').expect(200).then((response) => {
+					const article = response.body.article;
+					expect(article.author).toEqual(expect.any(String)),
+						expect(article.article_id).toBe(1),
+						expect(article.title).toEqual(expect.any(String)),
+						expect(article.topic).toEqual(expect.any(String)),
+						expect(article.created_at).toEqual(expect.any(String)),
+						expect(article.votes).toEqual(expect.any(Number));
+				});
+			});
+			test('returns an article by an ID number with a comment count column', () => {
+				return request(app).get('/api/articles/1').expect(200).then((response) => {
+					const article = response.body.article;
+					expect(article).toHaveProperty('comment_count');
+					expect(article.comment_count).toEqual(expect.any(Number));
+				});
+			});
+		});
+	});
+	describe('/api/articles/:article_id', () => {
+		describe('PATCH', () => {
+			test('Status 201: specified article_id article has been updated with votes incremented by passed object value', () => {
+				const inc_vote = { inc_vote: 5 };
+				return request(app)
+					.patch('/api/articles/1')
+					.send(inc_vote)
+					.expect(201)
+					.then((response) => {
+						expect(response.body.article.article_id).toBe(1);
+						expect(response.body.article.votes).toBe(105);
+					});
+			});
+			test('Status 201: specified article_id article has been updated with votes decremented by passed object value', () => {
+				const inc_vote = { inc_vote: -5 };
+				return request(app)
+					.patch('/api/articles/1')
+					.send(inc_vote)
+					.expect(201)
+					.then((response) => {
+						expect(response.body.article.article_id).toBe(1);
+						expect(response.body.article.votes).toBe(95);
+					});
+			});
+		});
+	});
+	describe('/api/users', () => {
+		describe('GET', () => {
+			test('Status 200: returns an array of users', () => {
+				return request(app).get('/api/users').expect(200).then((response) => {
+					expect(response.body.users).toEqual(expect.any(Array));
+				});
+			});
+			test('Status 200: returns an array of objects with the correct properties', () => {
+				return request(app).get('/api/users').expect(200).then((response) => {
+					const allUsers = response.body.users;
+					allUsers.forEach((user) => {
+						expect(user.hasOwnProperty('username')).toBe(true);
+						expect(user.hasOwnProperty('name')).toBe(true);
+						expect(user.hasOwnProperty('avatar_url')).toBe(true);
+					});
 				});
 			});
 		});
 	});
 });
 
-describe('/api/notARoute', () => {
-	describe('GET ERROR', () => {
-		test('Status 404: Not Found', () => {
-			return request(app).get('/api/notARoute').expect(404);
-		});
-	});
-});
-
-describe('/api/articles/:article_id', () => {
-	describe('GET', () => {
-		test('returns an article by an ID number', () => {
-			return request(app).get('/api/articles/1').expect(200).then((response) => {
-				const article = response.body.article;
-				expect(article.author).toEqual(expect.any(String)),
-					expect(article.article_id).toBe(1),
-					expect(article.title).toEqual(expect.any(String)),
-					expect(article.topic).toEqual(expect.any(String)),
-					expect(article.created_at).toEqual(expect.any(String)),
-					expect(article.votes).toEqual(expect.any(Number));
-			});
-		});
-		test('returns an article by an ID number with a comment count column', () => {
-			return request(app).get('/api/articles/1').expect(200).then((response) => {
-				const article = response.body.article;
-				expect(article.author).toEqual(expect.any(String)),
-					expect(article.article_id).toBe(1),
-					expect(article.title).toEqual(expect.any(String)),
-					expect(article.topic).toEqual(expect.any(String)),
-					expect(article.created_at).toEqual(expect.any(String)),
-					expect(article.votes).toEqual(expect.any(Number));
-				expect(article).toHaveProperty('comment_count');
-				expect(article.comment_count).toEqual(expect.any(Number));
+describe('ERROR HANDLING\n', () => {
+	describe('/api/notARoute', () => {
+		describe('GET ERROR', () => {
+			test('Status 404: Not Found', () => {
+				return request(app).get('/api/notARoute').expect(404);
 			});
 		});
 	});
-});
-
-describe('/api/articles/99(404) & /api/articles/notAnId(400)', () => {
-	describe('GET ERROR', () => {
-		test('Status 404: Not Found', () => {
-			return request(app).get('/api/articles/99').expect(404).then((response) => {
-				expect(response.body.msg).toBe('Article not found');
+	describe('/api/articles/99(404) & /api/articles/notAnId(400)', () => {
+		describe('GET ERROR', () => {
+			test('Status 404: Not Found', () => {
+				return request(app).get('/api/articles/99').expect(404).then((response) => {
+					expect(response.body.msg).toBe('Article not found');
+				});
 			});
-		});
-		test('Status 400: Bad request', () => {
-			return request(app).get('/api/articles/notAnId').expect(400).then((response) => {
-				expect(response.body.msg).toBe('Bad request');
-			});
-		});
-	});
-});
-
-describe('/api/articles/:article_id', () => {
-	describe('PATCH', () => {
-		test('Status 201: specified article_id article has been updated with votes incremented by passed object value', () => {
-			const inc_vote = { inc_vote: 5 };
-			return request(app).patch('/api/articles/1').send(inc_vote).expect(201).then((response) => {
-				expect(response.body.article.article_id).toBe(1);
-				expect(response.body.article.votes).toBe(105);
-			});
-		});
-		test('Status 201: specified article_id article has been updated with votes decremented by passed object value', () => {
-			const inc_vote = { inc_vote: -5 };
-			return request(app).patch('/api/articles/1').send(inc_vote).expect(201).then((response) => {
-				expect(response.body.article.article_id).toBe(1);
-				expect(response.body.article.votes).toBe(95);
+			test('Status 400: Bad request', () => {
+				return request(app).get('/api/articles/notAnId').expect(400).then((response) => {
+					expect(response.body.msg).toBe('Bad request');
+				});
 			});
 		});
 	});
-});
-
-describe('/api/articles/99(404) & /api/articles/notAnId(400)', () => {
-	describe('PATCH ERROR', () => {
-		test('Status 404: Not Found', () => {
-			return request(app).patch('/api/articles/99').expect(404).then((response) => {
-				expect(response.body.msg).toBe('Article not found');
+	describe('/api/articles/99(404) & /api/articles/notAnId(400)', () => {
+		describe('PATCH ERROR', () => {
+			test('Status 404: Not Found', () => {
+				return request(app).patch('/api/articles/99').expect(404).then((response) => {
+					expect(response.body.msg).toBe('Article not found');
+				});
 			});
-		});
-		test('Status 400: Bad request', () => {
-			return request(app).patch('/api/articles/notAnId').expect(400).then((response) => {
-				expect(response.body.msg).toBe('Bad request');
+			test('Status 400: Bad request', () => {
+				return request(app).patch('/api/articles/notAnId').expect(400).then((response) => {
+					expect(response.body.msg).toBe('Bad request');
+				});
 			});
-		});
-		test('Status 400: Bad request {inc_vote: banana}', () => {
-			const inc_vote = { inc_vote: 'banana' };
-			return request(app).patch('/api/articles/1').send(inc_vote).then((response) => {
-				expect(response.body.msg).toBe('Bad request');
-			});
-		});
-	});
-});
-
-describe('/api/users', () => {
-	describe('GET', () => {
-		test('Status 200: returns an array of users', () => {
-			return request(app).get('/api/users').expect(200).then((response) => {
-				expect(response.body.users).toEqual(expect.any(Array));
-			});
-		});
-		test('Status 200: returns an array of objects with the correct properties', () => {
-			return request(app).get('/api/users').expect(200).then((response) => {
-				const allUsers = response.body.users;
-				allUsers.forEach((user) => {
-					expect(user.hasOwnProperty('username')).toBe(true);
-					expect(user.hasOwnProperty('name')).toBe(true);
-					expect(user.hasOwnProperty('avatar_url')).toBe(true);
+			test('Status 400: Bad request {inc_vote: banana}', () => {
+				const inc_vote = { inc_vote: 'banana' };
+				return request(app).patch('/api/articles/1').send(inc_vote).then((response) => {
+					expect(response.body.msg).toBe('Bad request');
 				});
 			});
 		});
