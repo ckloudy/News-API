136b7bf6c91754ce9862f343e15020b9236df8e8
Damian Martin <dmartinr2d2@gmail.com>
2022-08-04 14:47:39 +0100

    this commit will add checkArticleExists function

--- models/articles.model.js
+++ models/articles.model.js
@@ -32,15 +32,20 @@ exports.selectArticleById = (id) => {
 		});
 };
 
+exports.checkArticleExists = (id) => {
+	return db.query(`SELECT * FROM articles WHERE article_id = $1`, [ id ]).then(({ rows }) => {
+		if (rows.length === 0) {
+			return Promise.reject({ status: 404, msg: 'Article not found' });
+		}
+	});
+};
+
 exports.selectCommentsForArticleId = (id) => {
 	return db
 		.query(`SELECT comment_id, votes, created_at, author, body FROM comments WHERE article_id = $1;`, [
 			id
 		])
 		.then(({ rows }) => {
-			if (rows.length === 0 || rows === undefined) {
-				return Promise.reject({ status: 404, msg: 'Article not found' });
-			}
 			return rows;
 		});
 };
