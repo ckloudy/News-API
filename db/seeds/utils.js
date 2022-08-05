0dafbc9edb002fab3dcc5b631a0b479b6bdfba49
Damian Martin <dmartinr2d2@gmail.com>
2022-08-05 16:11:23 +0100

    this commit will move util functions to utils.js

--- db/seeds/utils.js
+++ db/seeds/utils.js
@@ -1,22 +1,48 @@
+const db = require('../connection');
+
 exports.convertTimestampToDate = ({ created_at, ...otherProperties }) => {
-  if (!created_at) return { ...otherProperties };
-  return { created_at: new Date(created_at), ...otherProperties };
+	if (!created_at) return { ...otherProperties };
+	return { created_at: new Date(created_at), ...otherProperties };
 };
 
 exports.createRef = (arr, key, value) => {
-  return arr.reduce((ref, element) => {
-    ref[element[key]] = element[value];
-    return ref;
-  }, {});
+	return arr.reduce((ref, element) => {
+		ref[element[key]] = element[value];
+		return ref;
+	}, {});
 };
 
 exports.formatComments = (comments, idLookup) => {
-  return comments.map(({ created_by, belongs_to, ...restOfComment }) => {
-    const article_id = idLookup[belongs_to];
-    return {
-      article_id,
-      author: created_by,
-      ...this.convertTimestampToDate(restOfComment),
-    };
-  });
+	return comments.map(({ created_by, belongs_to, ...restOfComment }) => {
+		const article_id = idLookup[belongs_to];
+		return {
+			article_id,
+			author: created_by,
+			...this.convertTimestampToDate(restOfComment)
+		};
+	});
+};
+
+exports.checkArticleExists = (id) => {
+	return db.query(`SELECT * FROM articles WHERE article_id = $1`, [ id ]).then(({ rows }) => {
+		if (rows.length === 0) {
+			return Promise.reject({ status: 404, msg: 'Article not found' });
+		}
+	});
+};
+
+exports.checkTopic = (topic) => {
+	return db.query(`SELECT * FROM topics WHERE slug = $1`, [ topic ]).then(({ rows }) => {
+		if (rows.length === 0) {
+			return Promise.reject({ status: 404, msg: 'Topic not found' });
+		}
+	});
+};
+
+exports.checkUserExists = (username) => {
+	return db.query(`SELECT * FROM users WHERE username = $1`, [ username ]).then(({ rows }) => {
+		if (rows.length === 0) {
+			return Promise.reject({ status: 404, msg: 'Must be logged in' });
+		}
+	});
 };
