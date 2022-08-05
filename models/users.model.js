63a9833f74866406c7b705109019bb783a4eb45b
Damian Martin <dmartinr2d2@gmail.com>
2022-08-05 10:20:08 +0100

    this commit will add check user exists model function

--- models/users.model.js
+++ models/users.model.js
@@ -1,5 +1,13 @@
 const db = require('../db/connection');
 
+exports.checkUserExists = (username) => {
+	return db.query(`SELECT * FROM users WHERE username = $1`, [ username ]).then(({ rows }) => {
+		if (rows.length === 0) {
+			return Promise.reject({ status: 404, msg: 'Must be logged in' });
+		}
+	});
+};
+
 exports.selectUsers = () => {
 	return db.query(`SELECT * FROM users`).then(({ rows: users }) => {
 		return users;
