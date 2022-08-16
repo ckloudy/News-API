caa3b03e59ad3780932a2490fb2854e330f0e767
Damian Martin <dmartinr2d2@gmail.com>
2022-08-16 18:09:35 +0100

    this commit will add controller function for comment delete

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -1,60 +1,81 @@
 const {
-	selectArticles,
-	selectArticleById,
-	updatedVotesByArticleById,
-	selectCommentsForArticleId,
-	insertCommentOnArticleId
+  selectArticles,
+  selectArticleById,
+  updatedVotesByArticleById,
+  selectCommentsForArticleId,
+  insertCommentOnArticleId,
+  removeComment
 } = require('../models/articles.model');
 
-const { checkArticleExists, checkTopic, checkUserExists } = require('../db/seeds/utils');
+const {
+  checkArticleExists,
+  checkTopic,
+  checkUserExists
+} = require('../db/seeds/utils');
 
 exports.getArticles = (req, res, next) => {
-	const { sort_by, order, topic } = req.query;
-	const promiseArray = [ selectArticles(sort_by, order, topic) ];
-	if (topic !== undefined) {
-		promiseArray.push(checkTopic(topic));
-	}
-	Promise.all(promiseArray)
-		.then((articles) => {
-			res.status(200).send({ articles: articles[0] });
-		})
-		.catch(next);
+  const { sort_by, order, topic } = req.query;
+  const promiseArray = [ selectArticles(sort_by, order, topic) ];
+  if (topic !== undefined) {
+    promiseArray.push(checkTopic(topic));
+  }
+  Promise.all(promiseArray)
+    .then((articles) => {
+      res.status(200).send({ articles: articles[0] });
+    })
+    .catch(next);
 };
 
 exports.getArticleById = (req, res, next) => {
-	const { id } = req.params;
-	selectArticleById(id)
-		.then((article) => {
-			res.status(200).send({ article });
-		})
-		.catch(next);
+  const { id } = req.params;
+  selectArticleById(id)
+    .then((article) => {
+      res.status(200).send({ article });
+    })
+    .catch(next);
 };
 
 exports.getCommentsForArticleId = (req, res, next) => {
-	const { id } = req.params;
-	Promise.all([ checkArticleExists(id), selectCommentsForArticleId(id) ])
-		.then(([ , comments ]) => {
-			res.status(200).send({ comments });
-		})
-		.catch(next);
+  const { id } = req.params;
+  Promise.all([ checkArticleExists(id), selectCommentsForArticleId(id) ])
+    .then(([ , comments ]) => {
+      res.status(200).send({ comments });
+    })
+    .catch(next);
 };
 
 exports.updateArticleById = (req, res, next) => {
-	const { inc_vote } = req.body;
-	const { id } = req.params;
-	updatedVotesByArticleById(id, inc_vote)
-		.then((article) => {
-			res.status(201).send({ article });
-		})
-		.catch(next);
+  const { inc_vote } = req.body;
+  const { id } = req.params;
+  updatedVotesByArticleById(id, inc_vote)
+    .then((article) => {
+      res.status(201).send({ article });
+    })
+    .catch(next);
 };
 
 exports.addCommentOnArticleId = (req, res, next) => {
-	const { id } = req.params;
-	const { username } = req.body;
-	Promise.all([ checkUserExists(username), checkArticleExists(id), insertCommentOnArticleId(id, req.body) ])
-		.then(([ , , comment ]) => {
-			res.status(201).send({ comment });
-		})
-		.catch(next);
+  const { id } = req.params;
+  const { username } = req.body;
+  Promise.all([
+    checkUserExists(username),
+    checkArticleExists(id),
+    insertCommentOnArticleId(id, req.body)
+  ])
+    .then(([ , , comment ]) => {
+      res.status(201).send({ comment });
+    })
+    .catch(next);
+};
+
+exports.removeCommentById = (req, res, next) => {
+  const { id } = req.params;
+  removeComment(id)
+    .then(() => {
+      res.status(204).send();
+    })
+    .catch((err) => {
+      console.log(err);
+      next();
+    });
 };
