ea433a79e2d30fd697a09f4955801526d5369045
Damian Martin <dmartinr2d2@gmail.com>
2022-08-17 15:54:32 +0100

    this commit will adjust endpoint path

--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -1,15 +1,11 @@
 const fs = require('fs');
 
 exports.getEndpoints = (req, res, next) => {
-  fs.readFile(
-    '/Users/damianmartin/Northcoders/backend/be-nc-news/endpoints.json',
-    'utf8',
-    (err, data) => {
-      if (err) {
-        next(err);
-      }
-      const parsedData = JSON.parse(data);
-      res.status(200).send(parsedData);
+  fs.readFile('./endpoints.json', 'utf8', (err, data) => {
+    if (err) {
+      next(err);
     }
-  );
+    const parsedData = JSON.parse(data);
+    res.status(200).send({ endpoints: parsedData });
+  });
 };
