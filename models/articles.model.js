f32149bc601f3c011f643851830d43b70a42552e
Damian Martin <dmartinr2d2@gmail.com>
2022-08-17 17:29:15 +0100

    this commit will add setup for production env

--- models/articles.model.js
+++ models/articles.model.js
@@ -86,7 +86,7 @@ exports.insertCommentOnArticleId = (id, newComment) => {
 
 exports.removeComment = (id) => {
   return db
-    .query(`DELETE FROM comments WHERE comment_id = $1 RETURNING*`, [ id ])
+    .query(`DELETE FROM comments WHERE comment_id = $1`, [ id ])
     .then(({ rows }) => {
       return rows;
     });
